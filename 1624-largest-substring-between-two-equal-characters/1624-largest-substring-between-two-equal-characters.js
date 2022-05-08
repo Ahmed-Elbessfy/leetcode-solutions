8ad1ecbe10fed8137d8d29c3b606441bbcd9449c
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-05-08 09:52:01 +0200

    Time: 70 ms (75.78%), Space: 42.1 MB (88.28%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -8,10 +8,8 @@ var maxLengthBetweenEqualCharacters = function(s) {
     for(let i = 0; i < s.length; i++){
         let last = s.lastIndexOf(s[i])
         if(i != last){
-            // console.log("max change")
             max = Math.max(max, last - i)
         }
-        // console.log(i, last, max)
     }
     
     return max -1
