fbe8631da6eb30bfcb66b4bd05e2a59842245c15
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-05-19 16:04:36 +0200

    Time: 122 ms (15.26%), Space: 42.3 MB (55.93%) - LeetHub

---
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers/1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var getNoZeroIntegers = function(n) {
+    let i = 1
+    while(i != n){
+        let m = n - i
+        if(!String(m).includes("0") && !String(i).includes("0")) return [i,m]
+        
+        
+        i++
+    }
+};
