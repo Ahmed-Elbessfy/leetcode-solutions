da48093f512186de4e72d208eac670d874b6b9db
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-05-24 11:19:54 +0200

    Time: 5533 ms (27.42%), Space: 49.8 MB (95.16%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
@@ -4,13 +4,17 @@
  * @return {number[]}
  */
 var sumEvenAfterQueries = function(nums, queries) {
-    let res = new Array(queries.length)
-    for(let i = 0; i < queries.length; i++){
-        let df = queries[i][0], ind = queries[i][1]
-        nums[ind] += df
-        // console.log(df, ind, nums)
-        res[i] = nums.reduce((ac,n) => n % 2 == 0 ? ac += n : ac,0)
-        // console.log(res)
-    }
-    return res
+    // let res = new Array(queries.length)
+    // for(let i = 0; i < queries.length; i++){
+    //     let df = queries[i][0], ind = queries[i][1]
+    //     nums[ind] += df
+    //     // console.log(df, ind, nums)
+    //     res[i] = nums.reduce((ac,n) => n % 2 == 0 ? ac += n : ac,0)
+    //     // console.log(res)
+    // }
+    // return res
+    return  queries.map(query => {
+        nums[query[1]] += query[0]
+        return nums.reduce((ac,n) => n % 2 == 0 ? ac += n : ac,0)
+    })
 };
