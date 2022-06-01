6174c063836d1248171621460e2eb53a66a57b42
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-01 12:22:35 +0200

    Time: 141 ms (5.07%), Space: 47.9 MB (5.07%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -5,9 +5,12 @@
 var runningSum = function(nums) {
     let res = [], curr = 0
     // nums.forEach((n,i) => res.push(nums.slice(0,i+1).reduce((acc,cur) => acc + cur,0)))
-    nums.forEach(n => {
-        curr += n
-        res.push(curr)
-    })
-    return res
+    // nums.forEach(n => {
+    //     curr += n
+    //     res.push(curr)
+    // })
+    for(let i = nums.length -1; i >= 0; i--){
+        nums.splice(i, 1, nums.slice(0,i+1).reduce((acc,cur) => acc + cur,0))
+    }
+    return nums
 };
