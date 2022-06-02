ee80566e84ce0827abd7d2ed5d2929596fd3683f
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-02 15:34:07 +0200

    Time: 79 ms (85.00%), Space: 44.6 MB (63.75%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -3,20 +3,20 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-//     let rowN = matrix[0].length
-//     let res = []
+    let rowN = matrix[0].length
+    let res = []
    
-//     for( let i = 0; i < rowN; i++){
-//         res[i] = []
-//         for(let j = 0; j < matrix.length; j++){
-//             res[i][j] = matrix[j][i]
-//         }
-//     }
-//     return res
-    return Array.from(matrix[0], (_,row) => {
-        return Array.from(matrix, (_, col) => {
-            return matrix[col][row]
-        })
-    })
+    for( let i = 0; i < rowN; i++){
+        res[i] = []
+        for(let j = 0; j < matrix.length; j++){
+            res[i][j] = matrix[j][i]
+        }
+    }
+    return res
+    // return Array.from(matrix[0], (_,row) => {
+    //     return Array.from(matrix, (_, col) => {
+    //         return matrix[col][row]
+    //     })
+    // })
 
 };
