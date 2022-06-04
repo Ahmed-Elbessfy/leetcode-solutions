348231f031a5f11ee78db6fae2cbe2896bee5ae4
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-04 13:31:00 +0200

    Time: 193 ms (26.49%), Space: 50.4 MB (70.20%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
@@ -3,33 +3,14 @@
  * @return {boolean}
  */
 var checkValid = function(matrix) {
-    let stand = ""
-    for(let i = 1; i <= matrix.length; i++) stand += i
-//     for(let r = 0; r < matrix.length; r++){
-//         // row check 
-//         let curr = matrix[r].sort((a,b) => a - b).join("")
-//         if(stand!= curr) return false
-
-//     }
-//     // column check 
-//     for(let c = 0; c < matrix.length; c++){
-//         let tem = ""
-//         for(let r = 0; r < matrix.length; r++){
-//             tem += matrix[c][r]
-//         }
-//         if(tem != stand) return false
-//     }
-    
-    
-    
     for(let r  = 0; r < matrix.length; r++){
-        let row = [],col = []
+        let row = new Set(),col = new Set()
         for(let c = 0; c < matrix[0].length; c++){
-            row.push(matrix[r][c])
-            col.push(matrix[c][r])
+            row.add(matrix[r][c])
+            col.add(matrix[c][r])
         }
-        console.log(row, col)
-        if(row.sort((a,b) => a - b).join("") != stand || col.sort((a,b) => a - b).join("") != stand) return false
+
+        if(row.size != matrix[0].length || col.size != matrix.length) return false
     }
     return true
 };
