0e6194f8a7a582ed70c15df7d9392fdc6ef12667
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-06 10:44:08 +0200

    Time: 294 ms (39.73%), Space: 48.5 MB (53.31%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var triangularSum = function(nums) {
-    if(nums.length == 1) return nums[0]
     while(nums.length > 1){
         let newArr = []
         for(let i = 0; i < nums.length - 1; i++){
-            let newN = (nums[i] + nums[i+1]) % 10
-            newArr.push(newN)
+            newArr.push((nums[i] + nums[i+1]) % 10)
         }
         nums = newArr
     }
