0ac35e0fcd898f732b82b2744a1697a2ec2654f6
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-11 12:00:25 +0200

    Time: 136 ms (63.30%), Space: 49.5 MB (14.45%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.js
+++ 503-next-greater-element-ii/503-next-greater-element-ii.js
@@ -3,17 +3,24 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(nums) {
+    // duplicate nums length to perfrom the circular operation
     nums = [...nums, ...nums]
+    // store max value to spare at least one loop
     let max  = Math.max(...nums),res = []
+    // loop only to the middle of the new nums length (( the length of the original nums ))
     for(let i = 0; i < nums.length / 2; i++){
-        if(nums[i] == max) res.push(-1)
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[j] > nums[i]){
-                res.push(nums[j])
-                break;
+        // use the max value, if the current N is the max 
+        if(nums[i] == max) {
+            res.push(-1)
+        } else {
+            for(let j = i + 1; j < nums.length; j++){
+                if(nums[j] > nums[i]){
+                    res.push(nums[j])
+                    break;
+                }
             }
+
         }
-        
     }
     
     return res
