f7e7dc1950772057173c19633150b2c115db49e4
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-11 12:02:31 +0200

    Time: 173 ms (38.53%), Space: 49 MB (20.87%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.js
+++ 503-next-greater-element-ii/503-next-greater-element-ii.js
@@ -9,15 +9,14 @@ var nextGreaterElements = function(nums) {
     let max  = Math.max(...nums),res = []
     // loop only to the middle of the new nums length (( the length of the original nums ))
     for(let i = 0; i < nums.length / 2; i++){
-        // use the max value, if the current N is the max 
-        if(nums[i] == max) {
-            res.push(-1)
-        } else {
-            for(let j = i + 1; j < nums.length; j++){
-                if(nums[j] > nums[i]){
-                    res.push(nums[j])
-                    break;
-                }
+        // use the max value, if the current N is the max then push -1 to the ressult
+        if(nums[i] == max) res.push(-1)
+        // continue checking
+        for(let j = i + 1; j < nums.length; j++){
+            // if the current sub N > current N, then push it to the result and break the loop
+            if(nums[j] > nums[i]){
+                res.push(nums[j])
+                break;
             }
         }
     }
