b89ee7d83668e46976dea24156fde3c37aa971f0
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-13 18:07:37 +0200

    Time: 120 ms (16.41%), Space: 43.4 MB (78.23%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.js
+++ 496-next-greater-element-i/496-next-greater-element-i.js
@@ -4,20 +4,24 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let ans = []
+    let ans = [], max = Math.max(...nums2)
     for(let n of nums1){
+        if(n != max) {
+            let ind = nums2.indexOf(n),g = n
         
-        let ind = nums2.indexOf(n),g = n
-        
-        for(let i = ind+1; i < nums2.length; i++){
-            if(nums2[i] > g ) {
-                g = nums2[i]
-                break
+            for(let i = ind+1; i < nums2.length; i++){
+                if(nums2[i] > g ) {
+                    g = nums2[i]
+                    break
+                }
             }
+
+            if(g == n) ans.push(-1)
+            else ans.push(g)
+        } else {
+            ans.push(-1)
         }
         
-        if(g == n) ans.push(-1)
-        else ans.push(g)
     }
     return ans
 };
