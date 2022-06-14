abf91fe079a3d546d0aa1691836aab6f6166dbfe
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-14 17:30:33 +0200

    Time: 154 ms (22.18%), Space: 46.2 MB (36.83%) - LeetHub

--- 383-ransom-note/383-ransom-note.js
+++ 383-ransom-note/383-ransom-note.js
@@ -12,7 +12,6 @@ var canConstruct = function(ransomNote, magazine) {
     for(let l of magazine){
         hm.set(l, hm.get(l) + 1 || 1)
     }
-    // console.log(hr, hm, hm.get("b") >= hr.get("b"))
     for(let [l,c] of hr){
         if(!hm.get(l) || hm.get(l) < c) return false
     }
