e49eb2312a3e91a796e33dc31df22aea4e295757
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-06-27 17:55:37 +0200

    Time: 243 ms (82.91%), Space: 60.7 MB (52.39%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -3,11 +3,10 @@
  * @param {number} k
  * @return {number[][]}
  */
-const getDist = arr =>  Math.sqrt((arr[0] ** 2) + (arr[1] ** 2))
+
 var kClosest = function(points, k) {
     return points.sort((a,b) => {
-        let distA = getDist(a), distB = getDist(b)
-        if(distA > distB) return 1
+         if(a[0] ** 2 + a[1] ** 2 > b[0] ** 2 + b[1] ** 2) return 1
         else return -1
     }).slice(0, k )
 };
