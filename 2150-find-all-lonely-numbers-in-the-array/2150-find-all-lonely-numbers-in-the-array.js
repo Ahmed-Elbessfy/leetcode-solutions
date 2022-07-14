39744ada8ce4accc975931044d40283e49c804c2
Ahmed Elbessfy <ahmedelbessfy@gmail.com>
2022-07-14 11:55:09 +0200

    Time: 416 ms (90.00%), Space: 79.3 MB (82.50%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.js
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.js
@@ -3,18 +3,9 @@
  * @return {number[]}
  */
 var findLonely = function(nums) {
-    
-    let h = {}, lon = []
-    for(let n of nums){
-        h[n] = h[n] + 1 || 1
-    }
-
-    for(let i in nums){
-        let n = nums[i], next = n + 1, prev = n - 1
-        if(h[n] == 1 && !h[next] && !h[prev]){
-            lon.push(n)
-        }
-    }
-    return lon
-    
+    return nums.sort((a,b) => a-b).filter((n,i) => {
+         if(nums[i + 1] - nums[i] <= 1 || nums[i] - nums[i - 1] <= 1) return false;
+        return true;
+        
+    })
 };
